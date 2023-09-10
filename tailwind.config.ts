b735c9ce881824c89cb1a613a79392de1516668a
jang_yoonsu <shagrat8546@gmail.com>
2023-09-11 02:11:11 +0900

    Design : 글로벌 스타일 설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,26 @@
-import type { Config } from 'tailwindcss'
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        main: "#A374DB",
+        dark: "#121519",
+        light: "#d9d9d9",
+      },
+      fontFamily: {
+        content: ["Inter", "sans-serif"],
       },
     },
   },
-  plugins: [],
-}
-export default config
+  plugins: [require("tailwind-scrollbar-hide")],
+};
