77f06b2ea303f97fd3157ef65ec9b58c95e680b5
jang_yoonsu <shagrat8546@gmail.com>
2023-09-12 12:09:44 +0900

    Feat : 이미지 컴포넌트 구현

---
+++ src/components/Atoms/ImageFrame.tsx
@@ -0,0 +1,45 @@
+import Image from "next/image";
+import React from "react";
+import { twMerge } from "tailwind-merge";
+
+interface ImageFrameProps {
+  imageUrl: string;
+  imageAlt: string;
+  aspectRatio: number;
+}
+
+const ImageFrame = ({ imageUrl, imageAlt, aspectRatio }: ImageFrameProps) => {
+  const style = `aspect-[10/${aspectRatio * 10}]`;
+
+  return (
+    <div
+      className={twMerge(
+        `
+        relative 
+        flex
+        mt-[${aspectRatio * 100 + "%"}] 
+        overflow-hidden 
+        rounded-2xl
+        w-full
+        aspect-[10/6]`,
+        style
+      )}
+    >
+      <Image
+        src={imageUrl}
+        alt={imageAlt}
+        fill
+        sizes={"900px"}
+        className="
+          absolute 
+          inset-0 
+          w-full 
+          h-full
+          object-cover
+        "
+      />
+    </div>
+  );
+};
+
+export default ImageFrame;
