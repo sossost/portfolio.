84180fe501009440cb8c3999f5cacdb10434ce67
jang_yoonsu <shagrat8546@gmail.com>
2023-09-15 23:38:52 +0900

    NextCofing 이미지 도메인추가

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: [
+      "fog-of-war.s3.ap-northeast-2.amazonaws.com",
+      "tripsketchbucket.s3.ap-northeast-2.amazonaws.com",
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
