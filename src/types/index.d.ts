11f1247605d56f8c4c0234084c8a249a58b9a6a0
jang_yoonsu <shagrat8546@gmail.com>
2023-09-15 22:30:15 +0900

    Feat : 프로젝트 타입 추가

---
+++ src/types/index.d.ts
@@ -0,0 +1,14 @@
+export interface Project {
+  id: string;
+  name: string;
+  period: string;
+  thumbnail: string;
+  image: string;
+  introduction: string;
+  description: string;
+  tags: string[];
+  link?: string;
+  website?: string;
+  github: string;
+  testId?: string;
+}
