85760ca3b24db0c11ad3e9f00e380a239d232fc4
jang_yoonsu <shagrat8546@gmail.com>
2023-09-15 22:30:03 +0900

    Feat : MD렌더러 교체

--- src/components/Molecules/ProjectDetail.tsx
+++ src/components/Molecules/ProjectDetail.tsx
@@ -1,16 +1,17 @@
-import { useMdToHtml } from "@/hooks/useMdToHTML";
+import { useFetchMd } from "@/hooks/useFetchMd";
 import { useContext } from "react";
-import { Project } from "../../../types";
 import { ProjectModalContext } from "@/provider/ProjectModalProvider";
+import { Project } from "@/types";
+import { AiFillGithub, AiOutlineLink } from "react-icons/ai";
 
 import CloseButton from "../Atoms/CloseButton";
 import TagList from "./TagList";
 import Section from "../Atoms/Section";
 import Title from "../Atoms/Title";
 import ImageFrame from "../Atoms/ImageFrame";
-import { AiFillGithub, AiOutlineLink } from "react-icons/ai";
 import IconicAnchor from "./IconicAnchor";
 import HoverControlWrapper from "../Atoms/HoverControlWrapper";
+import MarkdownRenderer from "../Atoms/MarkdownRenderer";
 
 interface ProjectDetailProps {
   project: Project;
@@ -18,41 +19,51 @@ interface ProjectDetailProps {
 
 const ProjectDetail = ({ project }: ProjectDetailProps) => {
   const { handleModalClose } = useContext(ProjectModalContext);
-  const htmlContent = useMdToHtml("introduction");
+  const mdContent = useFetchMd(project?.link || null);
 
   return (
     <div className="py-10 flex flex-col gap-y-4">
       <HoverControlWrapper className="flex justify-end">
         <CloseButton onClick={handleModalClose} />
       </HoverControlWrapper>
-      <div className="flex flex-col gap-y-8">
-        <Title>{project.name}</Title>
+      <div className="flex flex-col gap-y-6">
+        <div className="flex gap-x-4 justify-between items-end">
+          <Title>{project.name}.</Title>
+          <h4>{project.period}</h4>
+        </div>
 
         <ImageFrame
-          imageUrl={project.thumbnail}
+          imageUrl={project.image || project.thumbnail}
           imageAlt={project.name}
           aspectRatio={0.6}
         />
 
-        <Section title="About">
-          <div
-            className="w-full"
-            dangerouslySetInnerHTML={{ __html: htmlContent }}
-          />
-        </Section>
+        <Section title="About.">{project.description}</Section>
 
-        <Section title="Technologies">
+        <Section title="Technologies.">
           <TagList tags={project.tags} />
         </Section>
 
-        <Section title="Links">
-          <IconicAnchor href={project.website} icon={AiOutlineLink}>
-            {project.website}
-          </IconicAnchor>
-          <IconicAnchor href={project.website} icon={AiFillGithub}>
+        <Section title="Links.">
+          {project.website && (
+            <div className="flex gap-4">
+              <IconicAnchor href={project.website} icon={AiOutlineLink}>
+                {project.website}
+              </IconicAnchor>
+              {project.testId && <p>테스트 계정 : {project.testId}</p>}
+            </div>
+          )}
+
+          <IconicAnchor href={project.github} icon={AiFillGithub}>
             {project.github}
           </IconicAnchor>
         </Section>
+
+        {mdContent && (
+          <Section title="Detail.">
+            <MarkdownRenderer markdownContent={mdContent} />
+          </Section>
+        )}
       </div>
     </div>
   );
