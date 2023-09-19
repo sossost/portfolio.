b2c86e8b7ff2cc2682502b0b47c36b31080e08cb
jang_yoonsu <shagrat8546@gmail.com>
2023-09-19 13:30:52 +0900

    Feat : og설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,13 @@ const mostserrat = Montserrat({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Ynnsuis's portfolio",
-  description: "Ynnsuis's portfolio",
+  description: "프론트 엔드 개발자 장윤수의 포트폴리오 사이트 입니다.",
+  openGraph: {
+    title: "Ynnsuis's portfolio",
+    description: "프론트 엔드 개발자 장윤수의 포트폴리오 사이트 입니다.",
+    images:
+      "https://tripsketchbucket.s3.ap-northeast-2.amazonaws.com/portfolio/ynnsuis_og.jpg",
+  },
 };
 
 export default function RootLayout({
