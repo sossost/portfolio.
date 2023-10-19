f991abf34879aaf8954266c2ba6a0e30c51ba519
jang_yoonsu <shagrat8546@gmail.com>
2023-10-20 00:14:50 +0900

    config 이미지 도메인 추가

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
     domains: [
       "fog-of-war.s3.ap-northeast-2.amazonaws.com",
       "tripsketchbucket.s3.ap-northeast-2.amazonaws.com",
+      "github.com",
+      "www.notion.so",
+      "file.notion.so",
     ],
   },
 };
