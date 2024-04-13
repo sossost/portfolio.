e0fc0a128001d12f72453a68c42ddbb51af8ed28
jang_yoonsu <shagrat8546@gmail.com>
2024-04-13 22:35:23 +0900

    feat : resume 메뉴 추가

--- src/constants/menu.ts
+++ src/constants/menu.ts
@@ -1,3 +1,8 @@
 import { LINK } from "./link";
 
-export const HEADER_MENU = [LINK.ABOUT, LINK.PROJECT, LINK.CONTACT];
+export const HEADER_MENU = [
+  LINK.ABOUT,
+  LINK.PROJECT,
+  LINK.RESUME,
+  LINK.CONTACT,
+];
